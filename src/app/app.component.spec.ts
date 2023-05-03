e39c54e4eb0286bd0bdb4bd4152ef947c93b7156
Bryan <bryanlon@yahoo.com>
2023-05-03 17:59:30 +0800

    Update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pokedex'`, () => {
+  it(`should have as title 'pokeApi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokedex');
+    expect(app.title).toEqual('pokeApi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pokedex app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('pokeApi app is running!');
   });
 });
