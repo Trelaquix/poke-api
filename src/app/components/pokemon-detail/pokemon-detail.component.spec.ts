9a29e4ef1818c9b7295ccde7d1177ec8213e0fa0
Bryan <bryanlon@yahoo.com>
2023-05-03 12:37:23 +0800

    Update angular to version 11

--- src/app/components/pokemon-detail/pokemon-detail.component.spec.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PokemonDetailComponent } from './pokemon-detail.component';
 
@@ -6,7 +6,7 @@ describe('PokemonDetailComponent', () => {
   let component: PokemonDetailComponent;
   let fixture: ComponentFixture<PokemonDetailComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PokemonDetailComponent ]
     })
